f948523dcf2f7b40f85efe56f26285eda09bbfb5
atlas <a.sproson@outlook.com>
2024-01-24 14:59:41 +0000

    Calls prisma server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+const prisma = new PrismaClient();
 
 app.get("/api/notes", async (req, res) => {
-  res.json({ message: "success!" });
+  const notes = await prisma.note.findMany();
+  // res.json({ message: "success!" });
+  res.json(notes);
 });
+
+// curl http://localhost:5000/api/notes
 
 app.listen(5000, () => {
   console.log(
